087217371dd9def876ef33766f5fd3491a596621
takeshi-iwanari <takeshi.iwanari@tier4.jp>
2022-09-23 18:18:33 +0900

    ci: add regression test (#12)
    * ci: add regression test
    
    Signed-off-by: takeshi.iwanari <takeshi.iwanari@tier4.jp>
    
    * ci: compare report contents
    
    Signed-off-by: takeshi.iwanari <takeshi.iwanari@tier4.jp>
    
    * ci: update CARET_analyze version
    
    Signed-off-by: takeshi.iwanari <takeshi.iwanari@tier4.jp>
    
    * ci: modify png file compare method
    
    Signed-off-by: takeshi.iwanari <takeshi.iwanari@tier4.jp>
    
    * ci: downgrade pandas version
    
    Signed-off-by: takeshi.iwanari <takeshi.iwanari@tier4.jp>
    
    * ci: add manual script
    
    Signed-off-by: takeshi.iwanari <takeshi.iwanari@tier4.jp>
    
    * ci: change process order to avoid CARET bug(event filter doesn't work at the first time)
    
    Signed-off-by: takeshi.iwanari <takeshi.iwanari@tier4.jp>
    
    Signed-off-by: takeshi.iwanari <takeshi.iwanari@tier4.jp>

---
+++ compare/compare.sh
@@ -0,0 +1,91 @@
+#!/bin/sh
+
+# Compare *.yaml, *.png (exclude *.html)
+# apt install -y imagemagick
+
+# shellcheck disable=SC2016,SC2181,SC2154
+# cspell:ignore messageflow, PHASH
+
+# set -e
+
+echo "[compare] ========== Result =========="
+
+echo "[compare] 1/3 file existence matches"
+diff "${report_dir_1}" "${report_dir_2}" -r \
+    --exclude=*.git --exclude=autoware_universe_node_diagram.png --exclude=*_0_messageflow.* |
+    grep Only >diff_existence.txt
+
+if [ -s diff_existence.txt ]; then
+    cat diff_existence.txt
+    echo "[ERROR][compare] ERROR. file existence doesn't match"
+    return 1
+else
+    echo "[compare] OK. file existence matches"
+fi
+
+echo "[compare] 2/3 yaml files"
+diff "${report_dir_1}" "${report_dir_2}" -r \
+    --exclude=*.html --exclude=*.git --exclude=*.png \
+    -I "filename_messageflow:*" \
+    >diff_yaml.txt
+
+if [ -s diff_yaml.txt ]; then
+    cat diff_yaml.txt
+    echo "[ERROR][compare] ERROR. yaml doesn't match"
+    return 1
+else
+    echo "[compare] OK. yaml matches"
+fi
+
+echo "[compare] 3/3 PNG files"
+if [ -e png_err ]; then
+    rm png_err
+fi
+
+(
+    cd "${report_dir_1}" || exit
+    find ./ -type f -name "*.png" \
+        -not -name "autoware_universe_node_diagram.png" -not -name "*_0_messageflow.png" \
+        -print0
+) |
+    xargs -0 -I{} sh -c ' \
+        diff=`convert -metric PHASH "${report_dir_1}"/{} "${report_dir_2}"/{} -compare -format "%[distortion]" info:` && \
+        if [ $diff != "inf" ]; then
+            result=`python3 -c "print(${diff} > 50.0)"` && \
+            if [ $result = True ]; then
+                echo {} ${diff}
+                touch png_err
+            fi
+        fi'
+
+if [ -e png_err ]; then
+    echo "[ERROR][compare] ERROR. PNG doesn't match"
+    return 1
+else
+    echo "[compare] OK. PNG matches"
+fi
+
+echo "[compare] ====== ALL OK =============="
+
+# (cd "${report_dir_1}"; find -type f -name "*.png" \
+#     -not -name "autoware_universe_node_diagram.png" -not -name "*_0_messageflow.png" \
+#     -print0) \
+#     | xargs -0 -I{} diff "${report_dir_1}"/{} "${report_dir_2}"/{} \
+#     >diff_png.txt
+# (cd "${report_dir_1}"; find -type f -name "*.yaml" \
+#     -print0) \
+#     | xargs -0 -I{} diff "${report_dir_1}"/{} "${report_dir_2}"/{} -I "filename_messageflow:" \
+#     >diff_yaml.txt
+
+# (cd "${report_dir_1}"; find -type f -name "*.png" \
+#     -not -name "autoware_universe_node_diagram.png" -not -name "*_0_messageflow.png" \
+#     -print0) \
+#     | xargs -0 -I{} sh -c ' \
+#         convert -resize 100x50 "${report_dir_1}"/{} a.png && \
+#         convert -resize 100x50 "${report_dir_2}"/{} b.png && \
+#         composite -compose difference a.png b.png diff.png && \
+#         diff=`identify -format "%[mean]" diff.png` && \
+#         result=`echo "$diff > 10" | bc` && \
+#         if [ $result -eq 1 ]; then
+#             echo {}
+#         fi'
---
+++ compare/makereport_and_compare.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+# shellcheck disable=SC1090
+
+# set -e
+
+WORK_DIR=$(pwd)/verify_work
+
+. /opt/ros/"$ROS_DISTRO"/setup.sh
+. ~/ros2_caret_ws/install/local_setup.sh
+
+# Create work directory
+if [ -e "${WORK_DIR}" ]; then
+    rm -rf "${WORK_DIR}"
+fi
+mkdir "${WORK_DIR}"
+cd "${WORK_DIR}" || exit
+
+# Download reference data
+wget -nv https://github.com/tier4/CARET_report/releases/download/20220831/autoware_launch_trace_20220826-105249_universe_rosbag.zip
+unzip autoware_launch_trace_20220826-105249_universe_rosbag.zip
+git clone https://github.com/tier4/CARET_report.git -b gh-pages reference_result
+
+# Create report
+script_path="$(pwd)"/../
+trace_data="$(pwd)"/autoware_launch_trace_20220826-105249_universe_rosbag
+sample_autoware_dir="$(pwd)"/../sample_autoware
+package_list_json=package_list.json
+target_path_json=target_path.json
+start_time=20
+duration_time=9999
+max_node_depth=2
+draw_all_message_flow=false
+export trace_data package_list_json target_path_json start_time duration_time max_node_depth draw_all_message_flow
+cd "${sample_autoware_dir}" || exit
+sh "${script_path}"/make_report.sh
+mv report_autoware_launch_trace_20220826-105249_universe_rosbag "${WORK_DIR}"/.
+
+# Compare result
+cd "${WORK_DIR}" || exit
+report_dir_1=./reference_result
+report_dir_2=./report_autoware_launch_trace_20220826-105249_universe_rosbag
+export report_dir_1 report_dir_2
+sh "${script_path}"/compare/compare.sh
