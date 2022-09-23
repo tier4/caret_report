f756a0722656143b562d1742d242ee2cf3cb138c
takeshi.iwanari <takeshi.iwanari@tier4.jp>
2022-09-23 16:57:20 +0900

    ci: change process order to avoid CARET bug(event filter doesn't work at the first time)
    Signed-off-by: takeshi.iwanari <takeshi.iwanari@tier4.jp>

--- compare/makereport_and_compare.sh
+++ compare/makereport_and_compare.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+# shellcheck disable=SC1090
+
 # set -e
 
-WORK_DIR=`pwd`/verify_work
+WORK_DIR=$(pwd)/verify_work
 
 . /opt/ros/"$ROS_DISTRO"/setup.sh
 . ~/ros2_caret_ws/install/local_setup.sh
@@ -12,7 +14,7 @@ if [ -e "${WORK_DIR}" ]; then
     rm -rf "${WORK_DIR}"
 fi
 mkdir "${WORK_DIR}"
-cd "${WORK_DIR}"
+cd "${WORK_DIR}" || exit
 
 # Download reference data
 wget -nv https://github.com/tier4/CARET_report/releases/download/20220831/autoware_launch_trace_20220826-105249_universe_rosbag.zip
@@ -20,21 +22,23 @@ unzip autoware_launch_trace_20220826-105249_universe_rosbag.zip
 git clone https://github.com/tier4/CARET_report.git -b gh-pages reference_result
 
 # Create report
-export script_path=`pwd`/../
-export trace_data=`pwd`/autoware_launch_trace_20220826-105249_universe_rosbag
-export sample_autoware_dir=`pwd`/../sample_autoware
-export package_list_json=package_list.json
-export target_path_json=target_path.json
-export start_time=20
-export duration_time=9999
-export max_node_depth=2
-export draw_all_message_flow=false
-cd ${sample_autoware_dir}
-sh ${script_path}/make_report.sh
+script_path="$(pwd)"/../
+trace_data="$(pwd)"/autoware_launch_trace_20220826-105249_universe_rosbag
+sample_autoware_dir="$(pwd)"/../sample_autoware
+package_list_json=package_list.json
+target_path_json=target_path.json
+start_time=20
+duration_time=9999
+max_node_depth=2
+draw_all_message_flow=false
+export trace_data package_list_json target_path_json start_time duration_time max_node_depth draw_all_message_flow
+cd "${sample_autoware_dir}" || exit
+sh "${script_path}"/make_report.sh
 mv report_autoware_launch_trace_20220826-105249_universe_rosbag "${WORK_DIR}"/.
 
 # Compare result
-cd "${WORK_DIR}"
-export report_dir_1=./reference_result
-export report_dir_2=./report_autoware_launch_trace_20220826-105249_universe_rosbag
-sh ${script_path}/compare/compare.sh
+cd "${WORK_DIR}" || exit
+report_dir_1=./reference_result
+report_dir_2=./report_autoware_launch_trace_20220826-105249_universe_rosbag
+export report_dir_1 report_dir_2
+sh "${script_path}"/compare/compare.sh
--- make_report.sh
+++ make_report.sh
@@ -7,6 +7,11 @@ set -e
 trace_data_name=$(basename "${trace_data}")
 report_dir_name=report_${trace_data_name}
 
+# Path analysis
+python3 "${script_path}"/analyze_path/add_path_to_architecture.py "${target_path_json}" --trace_data="${trace_data}" --max_node_depth="${max_node_depth}" -v
+python3 "${script_path}"/analyze_path/analyze_path.py "${trace_data}" architecture_path.yaml -s "${start_time}" -d "${duration_time}" -f -v -m "${draw_all_message_flow}"
+python3 "${script_path}"/analyze_path/make_report_path.py "${report_dir_name}"
+
 # Node analysis
 python3 "${script_path}"/analyze_node/analyze_node.py "${trace_data}" --package_list_json="${package_list_json}" -s "${start_time}" -d "${duration_time}" -f -v
 python3 "${script_path}"/analyze_node/make_report_node.py "${report_dir_name}"
@@ -16,11 +21,6 @@ python3 "${script_path}"/check_callback_sub/make_report_sub.py "${report_dir_nam
 
 python3 "${script_path}"/check_callback_timer/check_callback_timer.py "${trace_data}" --package_list_json="${package_list_json}" -s "${start_time}" -d "${duration_time}" -f -v
 python3 "${script_path}"/check_callback_timer/make_report_timer.py "${report_dir_name}"
-
-# Path analysis
-python3 "${script_path}"/analyze_path/add_path_to_architecture.py "${target_path_json}" --trace_data="${trace_data}" --max_node_depth="${max_node_depth}" -v
-python3 "${script_path}"/analyze_path/analyze_path.py "${trace_data}" architecture_path.yaml -s "${start_time}" -d "${duration_time}" -f -v -m "${draw_all_message_flow}"
-python3 "${script_path}"/analyze_path/make_report_path.py "${report_dir_name}"
 
 # Make top page
 python3 "${script_path}"/top/make_report_top.py "${report_dir_name}" --note_text_top note_text_top.txt --note_text_bottom note_text_bottom.txt
