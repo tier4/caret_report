7c29f50d4cac5c3616e116d87b7f978d5b9eb338
takeshi.iwanari <takeshi.iwanari@tier4.jp>
2022-12-13 10:59:10 +0900

    fix: lint
    Signed-off-by: takeshi.iwanari <takeshi.iwanari@tier4.jp>

--- validation/docker/entrypoint.sh
+++ validation/docker/entrypoint.sh
@@ -5,26 +5,28 @@ USER_ID=$(id -u)
 GROUP_ID=$(id -g)
 
 if [ x"$GROUP_ID" != x"0" ]; then
-    groupadd -g $GROUP_ID $USER_NAME
+    groupadd -g "$GROUP_ID" "$USER_NAME"
 fi
 
 if [ x"$USER_ID" != x"0" ]; then
-    useradd -d /home/$USER_NAME -m -s /bin/bash -u $USER_ID -g $GROUP_ID $USER_NAME
+    useradd -d /home/"$USER_NAME" -m -s /bin/bash -u "$USER_ID" -g "$GROUP_ID" "$USER_NAME"
 fi
-export HOME=/home/$USER_NAME
+export HOME=/home/"$USER_NAME"
 
 sudo chmod u-s /usr/sbin/useradd
 sudo chmod u-s /usr/sbin/groupadd
 # Add user and group: end
 
 # Add to .bashrc for VSCode
-echo "source /opt/ros/humble/setup.bash" >>$HOME/.bashrc
-echo "source /ros2_caret_ws/install/local_setup.bash" >>$HOME/.bashrc
-echo "source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.bash" >>$HOME/.bashrc
+{
+    echo "source /opt/ros/humble/setup.bash"
+    echo "source /ros2_caret_ws/install/local_setup.bash"
+    echo "source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.bash"
+} >>"$HOME"/.bashrc
 
 cd /work
 # work_dir="work_"`date +"%Y%m%d_%H%M%S"`
-# mkdir $work_dir
-# cd $work_dir
+# mkdir "$work_dir"
+# cd "$work_dir"
 
 exec "$@"
