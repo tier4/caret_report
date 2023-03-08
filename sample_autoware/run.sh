7b3c8a5751df0f14edbe8d1fb46221d0c2cf3543
takeshi.iwanari <takeshi.iwanari@tier4.jp>
2023-03-08 20:01:08 +0900

    fix(track_path): logic to search stats_file
    Signed-off-by: takeshi.iwanari <takeshi.iwanari@tier4.jp>

--- sample_autoware/run.sh
+++ sample_autoware/run.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# Create analysis report
 export component_list_json=./component_list.json
 export target_path_json=./target_path.json
 export max_node_depth=20
@@ -10,9 +11,12 @@ export note_text_top=./note_text_top.txt
 export note_text_bottom=./note_text_bottom.txt
 export start_strip=25
 export end_strip=0
+
 export trace_data=~/work/tracedata/universe/autoware_launch_trace_20221227-150253_universe_rosbag
 sh ../report/report_analysis/make_report.sh
 
+
+# Create validation report
 export component_list_json=./component_list.json
 export target_path_json=./target_path.json
 export max_node_depth=20
@@ -25,5 +29,6 @@ export note_text_top=./note_text_top.txt
 export note_text_bottom=./note_text_bottom.txt
 export start_strip=25
 export end_strip=0
+
 export trace_data=~/work/tracedata/universe/autoware_launch_trace_20221227-150253_universe_rosbag
 sh ../report/report_validation/make_report.sh
